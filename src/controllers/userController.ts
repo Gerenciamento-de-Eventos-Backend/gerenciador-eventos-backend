425edaff16b2c40bc5363f57c7b35340cea3a3bb
italo macedo <macedoitalo21@gmail.com>
2024-05-08 23:51:32 -0300

    Install @Prisma/client

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
