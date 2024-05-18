978c2b61f2d7c74497a3955e6e28e648abc7abff
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-05-18 15:56:40 -0300

    refactor: router productions

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { router } from "./router/productions"
+import { router } from "./router/user";
+import { productionsRouter } from "./router/productions";
 
 export const app = express();
 const port = 8000;
@@ -7,6 +8,7 @@ const port = 8000;
 app.use(express.json());
 
 app.use(router);
+app.use(productionsRouter)
 
 app.listen(port, () =>
     console.log(
--- src/router/productions.ts
+++ src/router/productions.ts
@@ -1,18 +1,18 @@
 import express from 'express';
 import { productionsController } from "../controllers/productionsController"
 
-export const router = express();
+export const productionsRouter = express();
 
 const production = new productionsController();
 
-router
+productionsRouter
     .route("/production")
     .post(production.create)
     .get(production.getAll)
     
-router.get("/production/:id", production.getOne);
-router.post("/production/:id", production.update);
-router.delete("/production/:id", production.delete);
+productionsRouter.get("/production/:id", production.getOne);
+productionsRouter.post("/production/:id", production.update);
+productionsRouter.delete("/production/:id", production.delete);
 
 
-export default router;
+export default productionsRouter;
