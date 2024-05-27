b1dfe8070a677ae4656552877d429a6bf88147e1
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-05-27 14:56:13 -0300

    feat: validate user

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,12 +1,12 @@
 import { Request, Response, NextFunction } from "express";
-import { production } from "../models/constructors/productions"
-import { ProductionSchema, ProductionInfer } from "../models/constructors/schemas/ProductionSchema"
+
+import { ProductionSchema, ProductionInfer } from "../models/constructors/schemas/ProductionSchema";
+import { userSchema } from "../models/constructors/schemas/userSchema";
 
 export const validateProductionCreate =  async (req: Request, res: Response, next:NextFunction) => {
     const productionData = req.body
  
     const validation = ProductionSchema.safeParse(productionData);
-    console.log(validation)
         
     if (!validation.success) {
         //validation.error.issues
@@ -22,4 +22,15 @@ export const validateProductionCreate =  async (req: Request, res: Response, nex
 //         }
 // }
 
+}
+
+export const validateUserCreate = async (req: Request, res: Response, next: NextFunction) => {
+    const userData = req.body;
+    const validation = userSchema.safeParse(userData);
+
+    if(!validation.success) {
+        const errorMessage = validation.error.issues.map(issue => issue.message);
+        return res.status(400).send(errorMessage)
+    }
+    next();
 }
